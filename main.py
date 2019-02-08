5961d6d15d1cb128d38969038d4297089793cbda
Ronaldo Vieira <ronaldoesv@gmail.com>
2019-02-08 15:51:54 -0200

    Refactors e-greedy into a function

--- main.py
+++ main.py
@@ -2,29 +2,27 @@ import numpy as np
 
 class Bandit:
 	def __init__(self, arms):
+		self.no_of_arms = arms
 		self.arms = [np.random.normal(0, 1) for _ in range(arms)]
 
 	def step(self, arm):
 		return np.random.normal(self.arms[arm], 1)
 
-def main():
-	no_of_arms = 10
-	max_steps = 1000
-	epsilon = 0.1
+def episilon_greedy(bandit, no_of_steps, epsilon, seed = None):
+	np.random.seed(seed)
 
-	bandit = Bandit(no_of_arms)
-	Q = [0] * no_of_arms
-	N = [0] * no_of_arms
+	Q = [0] * bandit.no_of_arms
+	N = [0] * bandit.no_of_arms
 
 	sum_of_rewards = 0
 
-	for _ in range(max_steps):
+	for _ in range(no_of_steps):
 		if np.random.random() > epsilon:
 			# greedy
 			action = np.argmax(Q)
 		else:
 			# random
-			action = np.random.choice(no_of_arms)
+			action = np.random.choice(bandit.no_of_arms)
 
 		reward = bandit.step(action)
 
@@ -33,10 +31,18 @@ def main():
 		N[action] += 1
 		Q[action] += (1 / N[action]) * (reward - Q[action])
 
-	print(sum_of_rewards / max_steps)
-	print(bandit.arms)
-	print(Q)
-	print(N)
+	mean_reward = sum_of_rewards / no_of_steps
+
+	return mean_reward, Q, N
+
+def main():
+	no_of_arms = 10
+	no_of_steps = 1000
+	epsilon = 0.1
+
+	bandit = Bandit(no_of_arms)
+	
+	print(episilon_greedy(bandit, no_of_steps, epsilon))
 
 
 if __name__ == '__main__':
